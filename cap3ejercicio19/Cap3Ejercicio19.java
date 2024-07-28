e88a5ee2256845eae3a856bb220f36a5f285e05f
juasasnchez <80784217+juasasnchez@users.noreply.github.com>
2024-07-27 21:44:56 -0500

    Add files via upload

---
+++ cap3ejercicio19/Cap3Ejercicio19.java
@@ -0,0 +1,10 @@
+package com.mycompany.cap3ejercicio19;
+
+public class Cap3Ejercicio19 {
+
+    public static void main(String[] args)  {
+        Grafica VentanaTriangulo = new Grafica();
+        VentanaTriangulo.show(true);
+
+    }   
+}
